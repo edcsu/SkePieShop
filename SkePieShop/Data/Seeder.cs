b2a26c5005e6c68c6042133811f4b9551147bcb3
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-03-15 04:46:48 +0300

    refactor: disable seeding pies

--- SkePieShop/Data/Seeder.cs
+++ SkePieShop/Data/Seeder.cs
@@ -10,18 +10,19 @@ public class Seeder
 
     private static void SeedData(AppDbContext context)
     {
-        if (!context.Pies.Any())
-        {
-            var pies = GenerateData.GetPiesList();
-            context.Pies.AddRange(pies);
-            context.SaveChanges();
-        }
-        
         if (!context.Categories.Any())
         {
             var categories = GenerateData.GetCategories();
             context.Categories.AddRange(categories);
             context.SaveChanges();
-        }
+        } 
+        
+        //
+        // if (!context.Pies.Any())
+        // {
+        //     var pies = GenerateData.GetPiesList();
+        //     context.Pies.AddRange(pies);
+        //     context.SaveChanges();
+        // }
     }
 }
